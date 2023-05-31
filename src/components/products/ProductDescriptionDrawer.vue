5dce950b42b8994abcf67efed09869cc8f0632d5
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-05-31 17:20:59 -0300

    Visualização de produto e adicionando e removendo no carrinho.

---
+++ src/components/products/ProductDescriptionDrawer.vue
@@ -0,0 +1,127 @@
+<template>
+  <div
+    class="drawer-background" 
+    :class="{ show: active }" 
+    @click="$emit('close-product-drawer')">
+  </div>
+  <div class="drawer" :class="{ show: active }">
+    <div class="drawer-close" @click="$emit('close-product-drawer')">
+      x
+    </div>
+    <div v-if="product" class="product-details">
+    <h3 class="text-center">{{ product.title }}</h3>
+    
+      <p class="description">{{ product.description }}</p>
+      <h3 class="text-center">${{ product.price }}</h3>
+
+      <div class="cart-total" v-if="productTotal">
+        <h3>In Cart</h3>
+        <h4>{{ productTotal }}</h4>
+      </div>
+
+      <div class="button-container">
+        <button class="remove" @click="removeFromCart">Remove</button>
+        <button class="add" @click="addToCart">Add</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default ({
+  name: 'ProductDescriptionDrawer',
+  props: ['product', 'active'],
+  computed: {
+    productTotal() {
+      return this.$store.getters.productQuantity(this.product)
+    }
+  },
+  methods: {
+    addToCart() {
+      this.$store.commit('addToCart', this.product)
+    },
+    removeFromCart() {
+      this.$store.commit('removeFromCart', this.product)
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.drawer-background {
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background-color: rgba(124, 124, 124, 0.55);
+  z-index: 100;
+  display: none;
+  transition: display .5s;
+
+  &.show {
+    display: block;
+  }
+}
+
+.drawer {
+  width: 80vw;
+  height: 100vh;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: -100vw;
+  padding: 15px;
+  transition: left .5s;
+  z-index: 101;
+  overflow-y: scroll;
+
+  &.show {
+    left: 0;
+  }
+}
+
+.drawer-close {
+  font-size: 1.5rem;
+  padding: 5px;
+  border-radius: 5px;
+  right: 10px;
+  border: 2px solid gray;
+  color: gray;
+  width: 15px;
+  float: right;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgray;
+  }
+}
+
+.product-details {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  p.description {
+    padding: 20px;
+    line-height: 1.5rem;
+  }
+
+  .button-container {
+    button {
+      width: 150px;
+      border: none;
+      padding: 10px;
+      border-radius: 5px;
+      margin: 0 5px 50px 5px;
+      cursor: pointer;
+    }
+  }
+}
+
+@media (min-width: 500px) {
+  .drawer {
+    width: 450px;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="home">
+    <ProductDescriptionDrawer 
+      :product="product"
+      :active="active.product_drawer"
+      @close-product-drawer="closeScrennProductDrawer"
+    />
 
     <div class="product-cards-container">
-      <ProductSummaryCard v-for="product in items" :key="product.id" :product="product" />
+      <ProductSummaryCard
+        v-for="product in items" 
+        :key="product.id" 
+        :product="product"
+        @view-product="viewProduct(product)"
+      />
     </div>
   </div>
 </template>
@@ -12,18 +22,32 @@ import { defineComponent } from 'vue';
 // @ is an alias to /src
 import items from '@/data/items';
 import ProductSummaryCard from '@/components/products/ProductSummaryCard.vue';
+import ProductDescriptionDrawer from '@/components/products/ProductDescriptionDrawer.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    ProductSummaryCard
+    ProductSummaryCard, ProductDescriptionDrawer
   },
   data() {
     return {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      items: items as Array<any>
+      items: items as Array<any>,
+      product: null,
+      active: {
+        product_drawer: false
+      }
     }
   },
+  methods: {
+    viewProduct(product): void {
+      this.product = product;
+      this.active.product_drawer = true;
+    },
+    closeScrennProductDrawer() {
+      this.active.product_drawer = false;
+    }
+  }
 });
 </script>
 
